7df8c1b3b5777e32a7d429264f1e3078c80020dc
vzhukovsky <jukovskiyv96@gmail.com>
2020-10-10 16:02:37 +0300

    finish project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module Dino.main {
     exports world.ucode.userinterface;
     exports world.ucode.objectgame;
-    exports world.ucode.utils;
+    exports world.ucode.util;
 
     requires java.base;
     requires java.desktop;
